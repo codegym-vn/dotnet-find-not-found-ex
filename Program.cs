697e33beb3f35cdfeacacffe1d679bf43d90bc43
Son Mai <son.mai@codegym.vn>
2020-03-12 15:20:05 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace dotnet_find_not_found_ex
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // Read in nonexistent file.
+                using (StreamReader reader = new StreamReader("not-there.txt"))
+                {
+                    reader.ReadToEnd();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                // Write error.
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
